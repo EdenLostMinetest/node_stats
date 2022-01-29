a31c9aa1240077b99087a0b7272fbde43dd482a3
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2022-01-28 21:40:26 -0800

    Now dumps details, one line per node, for easy grepping.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # node_stats
 
-A very crude mod to dump stats on how many nodes each of the other mods registered.
+A very crude mod to dump stats on how many nodes each of the other mods
+registered.
 
 Example from EdenLost (2022-01-28):
 
 ```
-$ cat worlds/world/node-stats.txt | sort -rn | head -n 20
+$ cat worlds/world/node-stats-summary.txt | sort -rn | head -n 20
 4658 moreblocks
 4203 scifi_nodes
 2250 cblocks
@@ -26,4 +27,17 @@ $ cat worlds/world/node-stats.txt | sort -rn | head -n 20
 318 street_signs
 285 ethereal
 269 farming
+```
+
+
+```
+$ cat worlds/world/node-stats-detail.txt | sort | grep pandorabox | head -n 3
+pandorabox_custom default:clay_technic_cnc_cylinder
+pandorabox_custom default:clay_technic_cnc_cylinder_horizontal
+pandorabox_custom default:clay_technic_cnc_element_cross
+
+$ cat worlds/world/node-stats-detail.txt | sort | grep "\?\?" | head -n 3
+3d_armor_stand 3d_armor_stand:armor_stand
+3d_armor_stand 3d_armor_stand:locked_armor_stand
+3d_armor_stand 3d_armor_stand:top
 ```
--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local function export_node_stats()
+local function export_summary_stats()
   local by_origin_mod = {}
 
   for mod_name, registration in pairs(minetest.registered_nodes) do
@@ -6,14 +6,29 @@ local function export_node_stats()
     by_origin_mod[origin] = (by_origin_mod[origin] or 0) + 1
   end
 
-  local fname = minetest.get_worldpath() .. "/node-stats.txt"
+  local fname = minetest.get_worldpath() .. "/node-stats-summary.txt"
   print("[node_stats] Exporting stats to: " .. fname)
   local f = io.open(fname, "w")
   for name, count in pairs(by_origin_mod) do
     f:write(count .. " " .. name .. "\n")
   end
-
   io.close(f)
+end
+
+local function export_details()
+  -- Dump full details.
+  local fname = minetest.get_worldpath() .. "/node-stats-detail.txt"
+  print("[node_stats] Exporting stats to: " .. fname)
+  local f = io.open(fname, "w")
+  for mod_name, registration in pairs(minetest.registered_nodes) do
+    f:write(registration.mod_origin .. " " .. mod_name .. "\n")
+  end
+  io.close(f)
+end
+
+local function export_node_stats()
+  export_summary_stats()
+  export_details()
 end
 
 minetest.after(4, export_node_stats)
